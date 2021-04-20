2f88de53f317b352a9f3da28b630b889d9476331
inflationmaker <56657974+superopgamemaker@users.noreply.github.com>
2021-04-20 13:05:59 -0700

    im sorry

--- main.js
+++ main.js
@@ -2,5 +2,7 @@
 var towerPoints = 1
 var builders = 0
 var boughtBuilders = 0
-gameLoop()
+var builderCost = 1
+gameLoop() {
+  var towerPoints = towerPoints + Math.(builders / Math.(1000/33))
 loop(gameLoop, 33)
