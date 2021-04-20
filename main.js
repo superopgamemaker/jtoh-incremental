1e018c16ba1a62344112c3558480ef7d2ca4a029
inflationmaker <56657974+superopgamemaker@users.noreply.github.com>
2021-04-20 14:06:26 -0700

    go

--- main.js
+++ main.js
@@ -3,13 +3,13 @@ var towerPoints = 1
 var builders = 0
 var boughtBuilders = 0
 var builderCost = 1
-gameLoop() {
-  var towerPoints = towerPoints + (builders / (1000/33));
+function gameLoop() {
+  var towerPoints = towerPoints + (builders / (1000/33))
 document.getElementById("towerPointStatement").innerHTML = "You have " + towerPoints + " tower points, corresponding to a difficulty of Easy"
   }
-loop(gameLoop, 33)
-buyBuilder() {
-  if towerPoints => builderCost {
+setInterval(gameLoop, 33)
+ function buyBuilder() {
+  if (towerPoints >= builderCost) {
     var towerPoints = towerPoints - builderCost
     var builders = builders + 1
     var boughtBuilders = boughtBuilders + 1
