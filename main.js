62732aff25907dd5e0aa51c95319fd3fad7daf89
inflationmaker <56657974+superopgamemaker@users.noreply.github.com>
2021-04-21 13:48:23 -0700

    yes

--- main.js
+++ main.js
@@ -4,12 +4,16 @@ var towerPoints = ON(1)
 var builders = ON(0)
 var boughtBuilders = ON(0)
 var builderCost = ON(1)
+var builderMultiplier = ON(1)
 var applications = ON(0)
 var boughtApplications = ON(0)
 var applicationCost = ON(100)
+var applicationMultiplier = ON(1)
+var upgrade1 = ON(0)
+var upgrade1Cost = ON(5)
 function gameLoop() {
-  towerPoints = towerPoints.plus(builders.div(1000/33))
-  builders = builders.plus(applications.div(1000/33))
+  towerPoints = towerPoints.plus(builders.div(1000/33).mul(builderMultiplier))
+  builders = builders.plus(applications.div(1000/33).mul(applicationMultiplier))
 document.getElementById("towerPointStatement").innerHTML = "You have " + towerPoints.notation() + " tower points, corresponding to a difficulty of Easy"
   }
 setInterval(gameLoop, 33)
@@ -19,6 +23,7 @@ setInterval(gameLoop, 33)
    builders = builders.plus(1)
    boughtBuilders = boughtBuilders.plus(1)
    builderCost = ON.pow(10, boughtBuilders.pow(2))
+   builderMultiplier = ON.pow(((upgrade1.div(5)).plus(1)), boughtBuilders)
     document.getElementById("buyBuilderButton").innerHTML = "Buy a builder for " + builderCost.notation() + " tower points"
     }
 }
@@ -28,6 +33,17 @@ function buyApplication() {
    applications = applications.plus(1)
    boughtApplications = boughtApplications.plus(1)
    applicationCost = ON(100).mul((ON.pow(10, boughtApplications.pow(2))).pow(2))
+   applicationMultiplier = ON.pow(((upgrade1.div(5)).plus(1)), boughtApplications)
     document.getElementById("buyApplicationButton").innerHTML = "Buy an application for " + applicationCost.notation() + " tower points"
+    }
+}
+function buyUpgrade1() {
+  if (towerPoints.gte(upgrade1Cost)) {
+   towerPoints = towerPoints.minus(upgrade1Cost)
+   upgrade1 = upgrade1.plus(1)
+   upgrade1Cost = ON(5).mul((ON.pow(10, boughtBuilders.pow(2)))
+   builderMultiplier = ON.pow(((upgrade1.div(5)).plus(1)), boughtBuilders)
+   applicationMultiplier = ON.pow(((upgrade1.div(5)).plus(1)), boughtApplications)
+   document.getElementById("buyUpgrade1Button").innerHTML = "Buy a builder for " + builderCost.notation() + " tower points"
     }
 }
